2ca99fc9511bd3b35e3f2c4122c76e6713208396
wadie19 <rezgui.m.w40@gmail.com>
2020-12-25 11:56:30 +0100

    menu ajouté

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { ListmaterielComponent } from './composants/informatique/listmateriel/li
 import { MaterielComponent } from './composants/informatique/materiel/materiel.component';
 import { DetailmaterielComponent } from './composants/informatique/detailmateriel/detailmateriel.component';
 import { CommentComponent } from './composants/commentaires/comment/comment.component';
+import { ContactComponent } from './composants/contact/contact.component';
+import { Error404Component } from './composants/error404/error404.component';
+import { MenuComponent } from './composants/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { CommentComponent } from './composants/commentaires/comment/comment.comp
     ListmaterielComponent,
     MaterielComponent,
     DetailmaterielComponent,
-    CommentComponent
+    CommentComponent,
+    ContactComponent,
+    Error404Component,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
